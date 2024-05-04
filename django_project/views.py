b5db0ffed45983f673d78725e4dbc9f3287f6929
wekesadaniel <32316930-wekesadaniel@users.noreply.replit.com>
2024-05-04 16:16:30 +0000

    Initial commit

---
+++ django_project/views.py
@@ -0,0 +1,14 @@
+import requests
+from django.shortcuts import render
+
+
+def home(request):
+#using APIS 
+  response = requests.get('https://api.github.com/events')
+  data = response.json()
+  result = data[0]["repo"]
+#example 2
+  response2 = requests.get('https://dog.ceo/api/breeds/image/random')
+  data2 = response2.json()
+  result2 = data2["message"]
+  return render(request, 'templates/index.html', {'result': result, 'result2': result2}) 
